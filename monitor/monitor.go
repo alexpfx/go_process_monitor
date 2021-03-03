b49c2bd1268e6b78293c6312ab0aaabf77cd78f8
alexpfx <alexpfba@gmail.com>
2021-03-03 00:18:08 -0300

    print

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 	"strings"
 )
+const (
+	InfoColor    = "\033[1;34m%s\033[0m"
+	NoticeColor  = "\033[1;36m%v\033[0m"
+	WarningColor = "\033[1;33m%v\033[0m"
+	ErrorColor   = "\033[1;31m%v\033[0m"
+	DebugColor   = "\033[0;36m%s\033[0m"
+)
 
 type Monitor struct {
 	ServerChan  chan Msg
@@ -11,11 +18,13 @@ type Monitor struct {
 
 	//map[term][]process
 	queue map[string][]string
+	page  int
+	cache []string
 }
 
 func (m *Monitor) Start() {
 	m.queue = make(map[string][]string)
-
+	m.cache = make([]string, 0)
 	for {
 		m.monitoring()
 	}
@@ -31,25 +40,34 @@ func (m *Monitor) monitoring() {
 			m.unregister(ms.Process, ms.Term)
 		}
 	case line := <-m.ProcessChan:
-		pss := m.search(line)
-		spawnProcess(pss, line)
+		m.page++
+		m.cache = append(m.cache, line)
+		if m.page%20 == 0 {
+			for _, l := range m.cache {
+				fmt.Printf("%s\n", l)
+			}
+		}
+
+		pss, t := m.search(line)
+		spawnProcess(pss, line, t)
 	}
 
 }
 
-func spawnProcess(pss []string, line string) {
-	for i, s := range pss {
-		fmt.Printf("Ouvindo termo: %d %s", i, s)
+func spawnProcess(pss []string, line string, t string) {
+	for _, s := range pss {
+		fmt.Printf("Ouvindo termo: %s ps: %s", fmt.Sprintf(InfoColor, t), fmt.Sprintf(NoticeColor, s))
 	}
 }
 
-func (m Monitor) search(line string) (pss []string) {
+func (m Monitor) search(line string) (pss []string, t string) {
 	if len(m.queue) == 0 {
 		return
 	}
 	for term := range m.queue {
 		if strings.Contains(line, term) {
 			pss = m.queue[term]
+			t = term
 			return
 		}
 	}
