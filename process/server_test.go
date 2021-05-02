f1d381ae087529870797c855a1970f9816749a2f
alexandre.alessi <alexandre.alessi@dataprev.gov.br>
2021-05-02 00:30:29 -0300

    novo servico

--- process/server_test.go
+++ process/server_test.go
@@ -1,18 +1,11 @@
 package process
 
 import (
-	"log"
-	"net"
 	"testing"
 )
 
 func TestMonitor_RunAndServ_connection(t *testing.T) {
 
-	dial, _ := net.Dial("unix", "/tmp/pm_test846665418/test.socket")
-	count, err := dial.Write([]byte("Hello world"))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("writed ", count)
+
 
 }
